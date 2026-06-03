40713c268ad80c482d0ffcb89256c97dc55c2fa5
Theo <theia005@osloskolen.no>
2026-06-03 09:27:34 +0200

    1

--- backend/main.py
+++ backend/main.py
@@ -20,32 +20,16 @@ print("________________")
 
 print("Legger til vare")
 
-#c.execute("INSERT INTO Max1000_lager (vare, antall) VALUES (?, ?)",
-#          ("Melk", 1))
-#conn.commit()
-
-print("______________")
-c.execute("SELECT * FROM Max1000_lager")
-res = c.fetchall()
-print(res)
-
-print("______________")
-
-c.execute("SELECT * FROM Max1000_lager WHERE id = ?", (2, ))
-res = c.fetchall()
-print(res)
-
-print("______________")
 
 
-c.execute("DELETE FROM Max1000_lager WHERE id = ?", (4, ))
-conn.commit()
 
-c.execute("SELECT * FROM Max1000_lager")
-res = c.fetchall()
-print(res)
 
-print("______________")
+
+
+
+
+
+
 
 
 
